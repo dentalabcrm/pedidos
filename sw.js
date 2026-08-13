b4857fb56873011b7c70c9fbd086f015b1532844
dentalabcrm <chino.gaffuri@gmail.com>
2026-08-13 14:39:46 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
 //  - Todo lo demás (Firebase, APIs externas, WhatsApp, etc.): SIEMPRE va directo a la red,
 //    el service worker no lo toca.
 
-const CACHE_NAME = 'dentalab-pedidos-v19';
+const CACHE_NAME = 'dentalab-pedidos-v20';
 const APP_SHELL = [
   './',
   './index.html',
